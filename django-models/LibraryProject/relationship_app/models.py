a7201e0f1e6c055594f703660a926bc9ddde4ff7
Isac Yevisa <isacyevisa@gmail.com>
2025-07-26 13:25:14 +0300

    removed comments from a file

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,31 +1,30 @@
-# relationship_app/query_samples.py
+
 
 import os
 import django
 
-# Set up Django environment
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
+
 def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
     return Book.objects.filter(author=author)
 
-# List all books in a library
 def books_in_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.books.all()
 
-# Retrieve the librarian for a library
+
 def librarian_for_library(library_name):
-    # Adjusted to match the required format
+    
     return Librarian.objects.get(library__name=library_name)
 
 if __name__ == "__main__":
-    # Example usage
+    
     print(books_by_author("George Orwell"))
     print(books_in_library("Main Library"))
     print(librarian_for_library("Main Library"))
