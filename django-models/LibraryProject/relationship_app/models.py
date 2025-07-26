cab2754dbf0ad7107ba5b150407232c4349b681c
Isac Yevisa <isacyevisa@gmail.com>
2025-07-26 13:16:55 +0300

    changed a file

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,28 +1,32 @@
-from django.db import models
+# relationship_app/query_samples.py
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+import os
+import django
 
-    def __str__(self):
-        return self.name
+# Set up Django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
+django.setup()
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+from relationship_app.models import Author, Book, Library, Librarian
 
-    def __str__(self):
-        return self.title
+# Query all books by a specific author
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
+# List all books in a library
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
-    def __str__(self):
-        return self.name
+# Retrieve the librarian for a library
+def librarian_for_library(library_name):
+    # Adjusted to match the required format
+    return Librarian.objects.get(library__name=library_name)
 
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+if __name__ == "__main__":
+    # Example usage
+    print(books_by_author("George Orwell"))
+    print(books_in_library("Main Library"))
+    print(librarian_for_library("Main Library"))
 
-    def __str__(self):
-        return self.name
