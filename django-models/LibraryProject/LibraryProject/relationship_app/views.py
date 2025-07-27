c060ffed7c4de266e150cfbfcbc4776f04a57bee
Isac Yevisa <isacyevisa@gmail.com>
2025-07-27 14:25:00 +0300

    modified some files

--- django-models/LibraryProject/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/LibraryProject/relationship_app/views.py
@@ -1,16 +1,31 @@
-# relationship_app/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
-from django.shortcuts import render
-from django.views import DetailView
-from .models import Library
-from .models import Book
-# Function-Based View to list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+# Registration View
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after registration
+            return redirect('home')  # Redirect to a home page or another view
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 
-# Class-Based View to display details for a specific library
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+# Login View
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to a home page or another view
+    return render(request, 'relationship_app/login.html')
+
+# Logout View
+def user_logout(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
