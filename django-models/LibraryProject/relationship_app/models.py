5aa688db8a846cb46750b49f4b15156990efca43
Isac Yevisa <isacyevisa@gmail.com>
2025-07-27 07:14:23 +0300

    chnaged some files

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,22 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+class Library(models.Model):
+    name = models.CharField(max_length=100)  # Name of the library
+    location = models.CharField(max_length=255)  # Location of the library
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+    def __str__(self):
+        return self.name
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+class Author(models.Model):
+    name = models.CharField(max_length=100)  # Name of the author
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)  # Title of the book
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # Link to the Author model
+    library = models.ForeignKey(Library, related_name='books', on_delete=models.CASCADE)  # Link to the Library model
+    publication_year = models.IntegerField()  # Year the book was published
+
+    def __str__(self):
+        return self.title
