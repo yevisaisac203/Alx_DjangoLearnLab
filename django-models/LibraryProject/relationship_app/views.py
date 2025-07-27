c098da7c634fe3a22347a095394eb8f284d870f3
Isac Yevisa <isacyevisa@gmail.com>
2025-07-27 16:00:24 +0300

    modified and added some files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,33 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book
-from .models import Library
-from django.views.generic.detail import DetailView
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
 
-# Function-based view
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
 
-# Class-based view
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+def is_member(user):
+    return user.userprofile.role == 'Member'
 
-# Create your views here.
-def register_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # Log the user in after registration
-            return redirect('list_books')  # Redirect to books or home page
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
