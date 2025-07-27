45f9a29591b479c76fa98a62b7ae05b420f67f46
Isac Yevisa <isacyevisa@gmail.com>
2025-07-27 06:45:33 +0300

    changed some files

--- django-models/LibraryProject/relationship_app/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/relationship_app/views.py
@@ -7,10 +7,10 @@ from .models import Book, Library
 # Function-Based View to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-Based View to display details for a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
