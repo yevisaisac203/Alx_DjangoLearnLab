0d17de0cd0ba9eb87a8693f25912a627f0452def
Isac Yevisa <isacyevisa@gmail.com>
2025-07-27 16:33:56 +0300

    modified a file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import add_book, edit_book, delete_book  # Ensure these views are imported
+from . import views
+from .views import list_books
 
 urlpatterns = [
-    path('books/add/', add_book, name='add_book'),  # URL for adding a book
-    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),  # URL for editing a book
-    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),  # URL for deleting a book
+    path('books/', views.list_books, name='list_books'),  # FBV
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV
+     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+     path('add_book/', views.add_book, name='add_book'),   # Add Book View
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),  # Edit Book View
+    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
 ]
