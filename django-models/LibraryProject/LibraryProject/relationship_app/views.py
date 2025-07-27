d0ca0f78aa980fecdc489cc6db33ba8329f99165
Isac Yevisa <isacyevisa@gmail.com>
2025-07-27 15:13:01 +0300

    modified some files

--- django-models/LibraryProject/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/LibraryProject/relationship_app/views.py
@@ -1,31 +1,33 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login 
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book
+from .models import Library
+from django.views.generic.detail import DetailView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
-# Registration View
-def register(request):
+
+# Function-based view
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+# Create your views here.
+def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log in the user after registration
-            return redirect('home')  # Redirect to a home page or another view
+            login(request, user)  # Log the user in after registration
+            return redirect('list_books')  # Redirect to books or home page
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
-
-# Login View
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')  # Redirect to a home page or another view
-    return render(request, 'relationship_app/login.html')
-
-# Logout View
-def user_logout(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
