4de3f3e79aa77e5c680006a53d039c878ee0ddae
Isac Yevisa <isacyevisa@gmail.com>
2025-07-27 06:57:58 +0300

    changed some files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,23 +1,13 @@
-from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
+from .models import Book
+from django.views.generic import DetailView
+from .models import Library
 
-def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == 'Admin'
+def list_books(request):
+    books = Book.objects.all()  # Retrieve all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-def is_librarian(user):
-    return user.is_authenticated and user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.is_authenticated and user.userprofile.role == 'Member'
-
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
