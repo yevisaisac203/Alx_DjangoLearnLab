e989a0eb7a130fe0fc5e50eda364db6334cf66d3
Isac Yevisa <isacyevisa@gmail.com>
2025-07-27 07:45:39 +0300

    chnaged a file

--- django-models/LibraryProject/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import render
 from django.views import DetailView
-from .models import Book, Library
+from .models import Library
 
 # Function-Based View to list all books
 def list_books(request):
