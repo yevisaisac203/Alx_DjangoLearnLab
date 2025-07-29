9b35d914f25da20e709033c6e872a3354653f3f9
Isac Yevisa <isacyevisa@gmail.com>
2025-07-29 15:46:54 +0300

    modified a file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from .models import Article
+from .models import Book
+
+# View to list all books, protected by can_view permission
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def article_list(request):
