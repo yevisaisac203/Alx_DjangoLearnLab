a682fa06a02d504adf50160daeb45bee4ad3bae5
Isac Yevisa <isacyevisa@gmail.com>
2025-08-16 11:47:07 +0300

    configured my DB with postgre in settings .py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,13 +75,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'test_user',
+        'PASSWORD': 'test_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
-
-# Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
