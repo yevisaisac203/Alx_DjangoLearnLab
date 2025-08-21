b80edc2bb1181f085c13051459c527c80cb2d855
Isac Yevisa <isacyevisa@gmail.com>
2025-08-21 12:51:04 +0300

    structured my urls for comments

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from .views import UserLogoutView, UserLoginView, register, index, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-from .views import PostDetailView,comment_create,CommentUpdateView,CommentDeleteView
+from .views import PostDetailView,comment_create,CommentUpdateView,CommentDeleteView,CommentCreateView
 
 
 urlpatterns= [
@@ -24,5 +24,9 @@ urlpatterns= [
     path("posts/<int:pk>/comments/new/", comment_create, name="comment-create"),
     path("comments/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment-edit"),
     path("comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'), # pyright: ignore[reportUndefinedVariable]
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+
 
 ]
