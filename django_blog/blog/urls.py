f4a26d63f3269e9ad247e4ac9b68ff9d48f804bb
Isac Yevisa <isacyevisa@gmail.com>
2025-08-21 13:36:52 +0300

    added slug

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,34 +1,28 @@
 from django.urls import path
 from . import views
-from .views import UserLogoutView, UserLoginView, register, index, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-from .views import PostDetailView,comment_create,CommentUpdateView,CommentDeleteView,CommentCreateView,PostsByTagListView, PostSearchListView
+from .views import  UserLogoutView, UserLoginView, register, index,    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,CommentCreateView, CommentUpdateView, CommentDeleteView,    PostByTagListView, PostSearchListView
 
 
-urlpatterns= [
+urlpatterns = [
     path("", index, name="home"),
     path("register/", register, name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
     path("profile/", views.profile, name="profile"),
-    #posts URLS
-    path("post/", PostListView.as_view(), name="post-list"),
+
+    # Post URLs
+    path("posts/", PostListView.as_view(), name="post-list"),
     path("post/new/", PostCreateView.as_view(), name="post-create"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 
-    # Comment routes:
-    path('post/new/', PostCreateView.as_view(), name='post'),
-    path("posts/<int:pk>/comments/new/", comment_create, name="comment-create"),
-    path("comments/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment-edit"),
-    path("comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
-    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'), # pyright: ignore[reportUndefinedVariable]
-    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
-    path("tags/<str:tag_name>/", PostsByTagListView.as_view(), name="posts-by-tag"),
+    # Comment URLs
+    path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment-create"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
+    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
+
+    # Tags & Search
+    path("tags/<slug:tag_slug>/", PostByTagListView.as_view(), name="posts-by-tag"),
     path("search/", PostSearchListView.as_view(), name="post-search"),
-
-
 ]
